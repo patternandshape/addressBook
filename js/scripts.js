7a98d454740f733330ada18f76ed3194215f46a5
Molly Curtin and Sean Peerenboom <seanmp42@gmail.com>
2016-01-25 10:15:42 -0800

    add corrected javascript and additional specs

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,19 @@ Contact.prototype.fullName = function() {
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-  });
 
   $(".contact").last().click(function() {
   $("#show-contact").show();
   $("#show-contact h2").text(newContact.fullName());
   $(".first-name").text(newContact.firstName);
   $(".last-name").text(newContact.lastName);
+});
 });
 });
