34659d071a90cb8c889dc269391dc740b5731fc2
Molly Curtin <patternandshape@gmail.com>
2016-01-25 20:41:33 -0800

    add final addressBook html and working javascript for additional addresses

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Contact(firstName, lastName) {
+function Contact(firstName,lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
@@ -7,7 +7,6 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
 
 function Address(street, city, state, zip) {
   this.street = street;
@@ -21,30 +20,61 @@ Address.prototype.fullAddress = function() {
 }
 
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-zip">Zipcode</label>' +
+                                   '<input type="text" class="form-control new-zip">' +
+                                 '</div>');
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
+
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    var pStreet = $("input#street").val();
-    var pCity = $("input#city").val();
-    var pState = $("input#state").val();
-    var pZip = $("input#zip").val();
-    var newAddress = new Address(pStreet, pCity, pState, pZip);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedZip = $(this).find("input.new-zip").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip)
+      newContact.addresses.push(newAddress)
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
+
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-zip").val("");
 
-  $(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.fullName());
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
-  $(".street").text(newAddress.street);
-  $(".city").text(newAddress.city);
-  $(".state").text(newAddress.state);
-  $(".zip").text(newAddress.zip);
-});
-});
+  });
 });
